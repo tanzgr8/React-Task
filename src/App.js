03a8001f813c612da443317e5b1089a48d9278a0
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-05-24 13:53:36 +0530

    Add Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{ Component } from "react";
  class App extends Component{
      render(){
-         return  <h1>Hemllo</h1>;
+         return <div><h1>Hemllo</h1><p></p><h2>HEMLLOM 2</h2></div> 
      }
  }
 export default App;
