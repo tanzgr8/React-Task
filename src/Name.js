37979c7d294ddae0f528eaea94a7cd47e0951c84
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-05-25 15:34:04 +0530

    Add Hook

--- src/Name.js
+++ src/Name.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react'
-class Name extends Component{
-state={
-  name:"Taanzeel"
+import React,{useState} from 'react';
+var name1;
+
+function Name(){
+const name= useState("TAnzeel");
+const change=()=>{
+  name1=document.getElementById("name").value;
+  name[1](name1);
 }
-render() {
-  return (
-    <div>
-      <h1>Hemllo {this.state.name}</h1>
-    </div>
-  )
-}
+return(
+  <React.Fragment>
+    <h1> Name : {name[0]}</h1>
+   <input type="text" id="name"></input>
+    <button type="button" onClick={change}>ClickME</button>
+  </React.Fragment>
+);
 
 }
+
 export default Name;
