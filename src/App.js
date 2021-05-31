8a1fdbe50383d6733c1cfa8105cd3ea5c2716c4c
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-05-31 17:26:09 +0530

    Mutliple control

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
 
 export class App extends Component {
-state={
-  value:"Tanzeel"
-};
-handleChange=(e)=>{
-this.setState({value : e.target.value.toUpperCase()});
-}
+  state = {
+    name: "Tanzeel",
+    pass:""
+  };
+  handleChange = (e) => {
+   const value =e.target.name==="pass"?e.target.value.toUpperCase():e.target.value;
+   this.setState({[e.target.name]:value})
+  };
   render() {
     return (
-    <>
-    <form>
-      <input type="text" value={this.state.value} onChange={this.handleChange} />
-
-    </form>
-    </>);
+      <>
+        <form>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="text"
+              name="pass"
+              value={this.state.pass}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+      </>
+    );
   }
 }
 export default App;
