5153d49d2d68461a0ba2656640a63d6a3648cb9c
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-01 12:05:41 +0530

    Form Validation

--- src/App.js
+++ src/App.js
@@ -3,27 +3,59 @@ import React, { Component } from 'react'
 export default class App extends Component {
   constructor(props){
     super(props);
-    this.textInput=React.createRef();
     this.state={
-      value:""
+      name:"",
+      pass:"",
+      nameerr:"",
+      passerr:""
     };
   }
-  handleSubmit=e=>{
+  handleSubmit=(e)=>{
     e.preventDefault();
-    this.setState({value:this.textInput.current.value});
+    if(this.valid()){
+      alert("Form Submitted");
+      this.setState({
+        nameerr:"",
+        passerr:""
+      })
+    }
   }
-  
+  valid(){
+    if(!this.state.name.includes("@")&&this.state.pass.length<5)
+    {this.setState({
+      nameerr:"Invalid Email",
+      passerr:"Password Should Be atleast 6 characters long"
+    })
+  return false;}
+    else if(!this.state.name.includes("@")){
+      this.setState({
+        nameerr:"Invalid Email"
+      })
+      return false;
+    }
+    else if(this.state.pass.lenght<5){
+      this.setState({
+        passerr:"Password Should Be atleast 6 characters long"
+      })
+      return false;
+     } else{
+      return true;
+    }
+  }
   render() {
     return (
   <React.Fragment>
-    <h1>Hemlllo {this.state.value}</h1>
-    <form onSubmit={this.handleSubmit}>
-      Input Name <input type="text" ref={this.textInput}/>
-      <input type="submit" value="Submit"/>
+    <h1>Hemlllo </h1>
+    <form >
+      Input EmailId <input type="text"onChange={(e)=>{this.setState({name:e.target.value})}} />
+      <p>{this.state.nameerr}</p>
+      Input Pasword <input type="text" onChange={(e)=>{this.setState({name:e.target.value})}} />
+      <p>{this.state.passerr}</p>
+      <input type="submit" value="Submit" onClick={this.handleSubmit(e)}/>
     </form>
   </React.Fragment>
     )
-  }
+  } 
 }
 
 
