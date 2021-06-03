faf812c802eaba1c56bfef74dddfb5e78b4e4dd1
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-03 13:59:42 +0530

    made good looking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Name from './Name';
+import "./app.css"
 let data;
 
 export default class App extends Component {
@@ -23,6 +24,9 @@ Change=(e)=>{
 data=this.state;
 console.log(data);
 }
+edit=()=>{
+this.setState({send:false});
+}
 handleChange=e=>{
   this.setState({[e.target.name]:e.target.value});
 }
@@ -30,26 +34,42 @@ handleChange=e=>{
    const  renderName = () => {
      console.log(this.state.send);
       if (this.state.send) {
-        return <Name data={data}/>;
+        return <div><Name data={data}/><br/><button onClick={this.edit} className="btn btn-primary">EDIT</button></div>;
       } 
     }
     return (
     <React.Fragment>
-      <form>
-        <label>Name
-        <input type="text" name="name" onChange={this.handleChange}/>
-        </label>
-        <label>Country
-        <input type="text" name="country" onChange={this.handleChange}/>
-        </label>
-        <label>capital
-        <input type="text" name="capital" onChange={this.handleChange}/>
-        </label>
-        <button onClick={this.Change}>SUBMIT</button>
+      <div className="container ct">
+        <form><div class="mb-3 row">
+          <label className=" col-sm-2 col-form-label">Name</label>
+          <div className="col-sm-10">
+            <input type="text" name="name" className="form-control form-control-sm" onChange={this.handleChange} />
+          </div>
+        </div>
+        <div class="mb-3 row">
+        <label className=" col-sm-2 col-form-label" >Country</label>
+          <div class="col-sm-10">
+          <input t pe="text" name="country"  className="form-control form-control-sm" onChange={this.handleChange}/>
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <label className=" col-sm-2 col-form-label">Capital</label>
+          <div class="col-sm-10">
+            <input type="text" name="capital" className="form-control form-control-sm" onChange={this.handleChange} />
+          </div>
+        </div>
+        
+        
+        <button onClick={this.Change} className="  btn btn-primary mar">SUBMIT</button>
       </form>
+      </div>
+     <div className="container ct2">
      {
        renderName()
+
      }
+     </div>
+     
     </React.Fragment>
     )
   }
