91f54eba6b21152c2e68d10910973c40d54d90e9
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-04 11:19:24 +0530

    Made New Page (final)

--- src/App.js
+++ src/App.js
@@ -30,26 +30,26 @@ handleChange=e=>{
    const  renderName = () => {
      console.log(this.state.send);
       if (this.state.send) {
-        return <div className="container ct2"><Name data={data}/><br/><button onClick={this.edit} className="btn btn-primary">EDIT</button></div>;
+        return <div className="container ct2"><Name data={data}/><br/><button onClick={this.edit}  className="btn btn-primary">EDIT</button></div>;
       } else{
         return(<React.Fragment>
         <div className="container ct">
         <form><div class="mb-3 row">
           <label className=" col-sm-2 col-form-label">Name</label>
           <div className="col-sm-10">
-            <input type="text" name="name" className="form-control form-control-sm" onChange={this.handleChange} />
+            <input type="text" name="name" className="form-control form-control-sm" value={this.state.name} onChange={this.handleChange} />
           </div>
         </div>
         <div class="mb-3 row">
         <label className=" col-sm-2 col-form-label" >Country</label>
           <div class="col-sm-10">
-          <input t pe="text" name="country"  className="form-control form-control-sm" onChange={this.handleChange}/>
+          <input type="text" name="country"  className="form-control form-control-sm" value={this.state.country} onChange={this.handleChange}/>
           </div>
         </div>
         <div class="mb-3 row">
           <label className=" col-sm-2 col-form-label">Capital</label>
           <div class="col-sm-10">
-            <input type="text" name="capital" className="form-control form-control-sm" onChange={this.handleChange} />
+            <input type="text" name="capital" className="form-control form-control-sm"value={this.state.capital} onChange={this.handleChange} />
           </div>
         </div>
         
