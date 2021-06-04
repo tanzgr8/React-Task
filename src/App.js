5b63c415c9b3fe78296a0b32ac64d5930e513082
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-04 12:46:43 +0530

    Add Function Components

--- src/App.js
+++ src/App.js
@@ -1,73 +1,60 @@
-import React, { Component } from 'react'
-import Name from './Name';
+import React,{useState} from 'react';
+import Name from './Name'
 import "./app.css"
-let data;
+export default function App(){
+const [name,setName]=useState("");
+const [country,setCountry]=useState("");
+const [capital,setCapital]=useState("");
+const [send,setSend]=useState(false);
 
-export default class App extends Component {
-constructor(){
-super();
-this.state={
-name:"",
-country:"",
-capital:"",
-send:false
-}
-} 
-
-Change=(e)=>{
+  const Change=(e)=>{
   e.preventDefault();
- this.setState({send:true});
-data=this.state;
-console.log(data);
+setSend(true);
 }
-edit=()=>{
-this.setState({send:false});
+const Edit=()=>{
+  setSend(false);
 }
-handleChange=e=>{
-  this.setState({[e.target.name]:e.target.value});
-}
-  render() {
-   const  renderName = () => {
-     console.log(this.state.send);
-      if (this.state.send) {
-        return <div className="container ct2"><Name data={data}/><br/><button onClick={this.edit}  className="btn btn-primary">EDIT</button></div>;
-      } else{
-        return(<React.Fragment>
-        <div className="container ct">
-        <form><div class="mb-3 row">
-          <label className=" col-sm-2 col-form-label">Name</label>
-          <div className="col-sm-10">
-            <input type="text" name="name" className="form-control form-control-sm" value={this.state.name} onChange={this.handleChange} />
-          </div>
-        </div>
-        <div class="mb-3 row">
-        <label className=" col-sm-2 col-form-label" >Country</label>
-          <div class="col-sm-10">
-          <input type="text" name="country"  className="form-control form-control-sm" value={this.state.country} onChange={this.handleChange}/>
-          </div>
-        </div>
-        <div class="mb-3 row">
-          <label className=" col-sm-2 col-form-label">Capital</label>
-          <div class="col-sm-10">
-            <input type="text" name="capital" className="form-control form-control-sm"value={this.state.capital} onChange={this.handleChange} />
-          </div>
-        </div>
-        
-        
-        <button onClick={this.Change} className="  btn btn-primary mar">SUBMIT</button>
-      </form>
-      </div>
-     
-     
-    </React.Fragment>)
-      }
-    }
-    return (
-  
-      
-       renderName()
 
+  const  renderName = () => {
+     if (send) {
+       return <div className="container ct2"><Name name={name} country={country} capital={capital} /><br/><button onClick={Edit}  className="btn btn-primary">EDIT</button></div>;
+     } else{
+       return(<React.Fragment>
+       <div className="container ct">
+       <form><div class="mb-3 row">
+         <label className=" col-sm-2 col-form-label">Name</label>
+         <div className="col-sm-10">
+           <input type="text" name="name" className="form-control form-control-sm" value={name} onChange={(e)=>setName(e.target.value)} />
+         </div>
+       </div>
+       <div class="mb-3 row">
+       <label className=" col-sm-2 col-form-label" >Country</label>
+         <div class="col-sm-10">
+         <input type="text" name="country"  className="form-control form-control-sm" value={country} onChange={(e)=>{setCountry(e.target.value)}}/>
+         </div>
+       </div>
+       <div class="mb-3 row">
+         <label className=" col-sm-2 col-form-label">Capital</label>
+         <div class="col-sm-10">
+           <input type="text" name="capital" className="form-control form-control-sm"value={capital} onChange={(e)=>{setCapital(e.target.value)}} />
+         </div>
+       </div>
+       
+       
+       <button onClick={Change} className="  btn btn-primary mar">SUBMIT</button>
+     </form>
+     </div>
+    
+    
+   </React.Fragment>)
+     }
+   }
+   return (
+ 
      
-    )
-      }
-    }
+      renderName()
+
+    
+   )
+     }
+
--- src/Name.js
+++ src/Name.js
@@ -2,9 +2,9 @@ import React from 'react'
 const Name =(props)=>{
   return(<React.Fragment>
 <h1>You Entered:</h1>
-<h3>Name : {props.data.name}</h3>
-<h3>Country : {props.data.country}</h3>
-<h3>Capital : {props.data.capital}</h3>
+<h3>Name : {props.name}</h3>
+<h3>Country : {props.country}</h3>
+<h3>Capital : {props.capital}</h3>
 </React.Fragment>
   )
 }
