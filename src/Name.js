f62f9cf76a83cea452faee471d0f64593c1542a8
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-07 12:52:07 +0530

    Final Edit Add Router Dom

--- src/Name.js
+++ src/Name.js
@@ -1,11 +1,19 @@
-import React from 'react'
-const Name =(props)=>{
-  return(<React.Fragment>
-<h1>You Entered:</h1>
-<h3>Name : {props.name}</h3>
-<h3>Country : {props.country}</h3>
-<h3>Capital : {props.capital}</h3>
-</React.Fragment>
-  )
-}
+import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+const Name = (props) => {
+  return (
+    <React.Fragment>
+      <div className="container ct2">
+        <h1>You Entered:</h1>
+        <h3>Name : {props.name}</h3>
+        <h3>Country : {props.country}</h3>
+        <h3>Capital : {props.capital}</h3>
+
+        <Link className="btn btn-primary btn-lg" role="button" to="/">
+          EDIT
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+};
 export default Name;
