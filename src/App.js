42401db68ef5647c598460ba0ec81e39dbd75df7
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-09 15:35:43 +0530

    first Redux

--- src/App.js
+++ src/App.js
@@ -1,86 +1,12 @@
-import React, { useState } from "react";
-import Name from "./Name";
-import "./app.css";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-export default function App() {
-  const [name, setName] = useState("");
-  const [country, setCountry] = useState("");
-  const [capital, setCapital] = useState("");
-  const [send, setSend] = useState(false);
+import React from 'react'
+import Cake from './Components/Cake'
 
-  const Change = (e) => {
-    e.preventDefault();
-    setSend(true);
-  };
-  const Edit = () => {
-    setSend(false);
-  };
-
+function App() {
   return (
-    <main>
-      <Switch>
-        <Route
-          exact
-          path="/result"
-          render={(props) => (
-            <Name name={name} country={country} capital={capital} />
-          )}
-        />
-
-        <React.Fragment>
-          <div className="container ct">
-            <form>
-              <div class="mb-3 row">
-                <label className=" col-sm-2 col-form-label">Name</label>
-                <div className="col-sm-10">
-                  <input
-                    type="text"
-                    name="name"
-                    className="form-control form-control-sm"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div class="mb-3 row">
-                <label className=" col-sm-2 col-form-label">Country</label>
-                <div class="col-sm-10">
-                  <input
-                    type="text"
-                    name="country"
-                    className="form-control form-control-sm"
-                    value={country}
-                    onChange={(e) => {
-                      setCountry(e.target.value);
-                    }}
-                  />
-                </div>
-              </div>
-              <div class="mb-3 row">
-                <label className=" col-sm-2 col-form-label">Capital</label>
-                <div class="col-sm-10">
-                  <input
-                    type="text"
-                    name="capital"
-                    className="form-control form-control-sm"
-                    value={capital}
-                    onChange={(e) => {
-                      setCapital(e.target.value);
-                    }}
-                  />
-                </div>
-              </div>
-              <Link
-                className="btn btn-primary btn-lg"
-                role="button"
-                to="/result"
-              >
-                Submit
-              </Link>
-            </form>
-          </div>
-        </React.Fragment>
-      </Switch>
-    </main>
-  );
+    <div>
+      <Cake/>
+    </div>
+  )
 }
+
+export default App
---
+++ src/Components/Cake.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cake() {
+    return (
+        <div>
+            <h1>Cake</h1>
+            <button>ADD</button>
+        </div>
+    )
+}
+
+export default Cake
